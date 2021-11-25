69e9d3d0d36ec9978ca7499a908f20a1052bc996
Rishard C <rishard.c@hotmail.com>
2021-11-24 21:14:39 -0500

    A function that will return all the indices (zero-based positions) in the string where each character is found

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])){
+        return false;
+      }
+    }else if (array1[i] !== array2[i]){
+      return false
+    }
+  }
+  return true
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+console.log(`✅ Assertation Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ Assertation Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
