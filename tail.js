57d8ba472aefa46448d5f44d4f9708ac20027eb2
Rishard C <rishard.c@hotmail.com>
2021-11-29 14:31:47 -0500

    Dried up tail.js and created a test file.

--- tail.js
+++ tail.js
@@ -11,11 +11,9 @@ const tail = function(arr) {
   return last;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+/*const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+*/
+ module.exports = tail;
