28fa0023361d409ccbb38519750ecd335cc67308
Rishard C <rishard.c@hotmail.com>
2021-11-29 17:46:47 -0500

    export all utility function to our object in index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-
+const map = require('./map');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const letterPositons = require('./letterPostions');
+const findKey = require('./findKey');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
 
 module.exports = {
   head:   head,
